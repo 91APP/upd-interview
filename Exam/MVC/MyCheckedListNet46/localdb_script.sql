6544dcb7bcb45cb85330d393df0e005a3dc3aacf
Jesse Wang <jessewang@91app.com>
2022-04-26 11:59:42 +0800

    Initial data

--- Exam/MVC/MyCheckedListNet46/localdb_script.sql
+++ Exam/MVC/MyCheckedListNet46/localdb_script.sql
@@ -1,3 +1,47 @@
+
+USE [MyCheckList]
+GO
+
+/****** Object:  Table [dbo].[Members]    Script Date: 4/26/2022 11:54:43 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Members](
+	[MemberId] [int] IDENTITY(1,1) NOT NULL,
+	[MemberName] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED 
+(
+	[MemberId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+USE [MyCheckList]
+GO
+
+/****** Object:  Table [dbo].[Items]    Script Date: 4/26/2022 11:54:32 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Items](
+	[ItemId] [int] IDENTITY(1,1) NOT NULL,
+	[ItemName] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED 
+(
+	[ItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
 USE [MyCheckList]
 GO
 
@@ -28,46 +72,14 @@ ALTER TABLE [dbo].[CheckedItem] CHECK CONSTRAINT [FK_CheckedItem_Members_MemberI
 GO
 
 
-
 USE [MyCheckList]
 GO
 
-/****** Object:  Table [dbo].[Items]    Script Date: 4/26/2022 11:54:32 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Items](
-	[ItemId] [int] IDENTITY(1,1) NOT NULL,
-	[ItemName] [nvarchar](max) NULL,
- CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED 
-(
-	[ItemId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+INSERT INTO [dbo].[Items]
+           ([ItemName])
+     VALUES
+           ('Item 1'),('Item 2'),('Item 3'),('Item 4')
 GO
 
 
-
-USE [MyCheckList]
-GO
-
-/****** Object:  Table [dbo].[Members]    Script Date: 4/26/2022 11:54:43 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Members](
-	[MemberId] [int] IDENTITY(1,1) NOT NULL,
-	[MemberName] [nvarchar](max) NULL,
- CONSTRAINT [PK_Members] PRIMARY KEY CLUSTERED 
-(
-	[MemberId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
 
